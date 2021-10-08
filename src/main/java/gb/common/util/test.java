4695f3e3740777c25cf7d4c0c9c7c2c20478a20d
growin-infra <freeson01@growin.co.kr>
2021-10-08 15:11:20 +0900

    2110081511

--- src/main/java/gb/common/util/test.java
+++ src/main/java/gb/common/util/test.java
@@ -1,5 +1,10 @@
 package gb.common.util;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.apache.commons.codec.binary.Base64;
 import org.apache.log4j.Logger;
 
@@ -7,7 +12,24 @@ public class test {
 
 	public static void main(String[] args) {
 		
-	
+		Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        DateFormat df = new SimpleDateFormat("yyyy.MM.dd");
+        String today = df.format(cal.getTime());
+        System.out.println("today["+today+"]");
+        cal.add(Calendar.DATE, 7);
+        String sevend = df.format(cal.getTime());
+        System.out.println("sevend["+sevend+"]");
+
+        String band = "growin";
+        String instance = "10";
+        String lcn = band+"_"+instance+"_"+today+"_"+sevend;
+        System.out.println("lcn["+lcn+"]");
+        System.out.println("============== test license============================================================");
+        System.out.println(""+lcn+" encode["+Base64Util.encode(""+lcn+"", 3)+"]");
+//        System.out.println(""+lcn+" decode["+Base64Util.decode("V2pOS2RtUXliSFZZZWtWM1dIcEpkMDFxUlhWTlZFRjFUVVJvWmsxcVFYbE5VelI0VFVNMGVFNVJQVDA9", 3)+"]");
+        System.out.println(""+lcn+" decode["+Base64Util.decode(Base64Util.encode(""+lcn+"", 3), 3)+"]");
+        System.out.println("=======================================================================================");
 		
 //		System.out.println("encode2 20.7.31["+Base64Util.encode("SKhynix_1_2019.08.01_2020.07.31", 3)+"]");
 //		System.out.println("encode2 21.7.31["+Base64Util.encode("SKhynix_1_2020.08.01_2021.07.31", 3)+"]");
@@ -17,10 +39,11 @@ public class test {
 //		System.out.println("decode1["+Base64Util.decode("VlRCMGIyVlhOWEJsUmpoNFdIcEpkMDFxUlhWTlJHZDFUVVJHWmsxcVFYbE5hVFIzVG5rMGVrMVJQVDA9", 3)+"]");
 //		System.out.println("decode2["+Base64Util.decode("VlRCMGIyVlhOWEJsUmpoNFdIcEpkMDFVYTNWTlJHZDFUVVJHWmsxcVFYbE5RelIzVG5rMGVrMVJQVDA9", 3)+"]");
 //		System.out.println("decode3["+Base64Util.decode("VlRCMGIyVlhOWEJsUmpoNFdIcEpkMDFxUVhWTlJHZDFUVVJHWmsxcVFYbE5VelIzVG5rMGVrMVJQVDA9", 3)+"]");
-		System.out.println("decode3["+Base64Util.decode("WXpJeGFHTnVVbnBaTWpsNVdsWTRlRmg2U1hkTlZHdDFUVVJSZFUxRVJtWk5ha0Y1VFVNMGQwMTVOSHBOVVQwOQ==", 3)+"]");
+//		System.out.println("decode3["+Base64Util.decode("WXpJeGFHTnVVbnBaTWpsNVdsWTRlRmg2U1hkTlZHdDFUVVJSZFUxRVJtWk5ha0Y1VFVNMGQwMTVOSHBOVVQwOQ==", 3)+"]");
 		
 		System.out.println("SKhynix_1_2021.08.01_2022.07.31 encode["+Base64Util.encode("SKhynix_1_2021.08.01_2022.07.31", 3)+"]");
 		System.out.println("SKhynix_1_2021.08.01_2022.07.31 decode["+Base64Util.decode("VlRCMGIyVlhOWEJsUmpoNFdIcEpkMDFxUlhWTlJHZDFUVVJHWmsxcVFYbE5hVFIzVG5rMGVrMVJQVDA9", 3)+"]");
+		
 		
 		
 	}
